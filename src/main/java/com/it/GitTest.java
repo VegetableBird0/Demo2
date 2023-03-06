38f53c8579e818da45c2e0f1e0bb15d4023dde9a
jymk <865019038@qq.com>
2023-03-06 09:56:50 +0800

    four commit

--- src/main/java/com/it/GitTest.java
+++ src/main/java/com/it/GitTest.java
@@ -3,6 +3,8 @@ package com.it;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+        System.out.println("hello Git2!");
+        System.out.println("hello Git3!");
 
     }
 }
