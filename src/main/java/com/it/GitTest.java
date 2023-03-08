2a610421f74ee9ac829d337687de0c4aa8761d8b
jymk <865019038@qq.com>
2023-03-08 10:27:12 +0800

    github commit2

--- src/main/java/com/it/GitTest.java
+++ src/main/java/com/it/GitTest.java
@@ -7,5 +7,7 @@ public class GitTest {
         System.out.println("hello Git555!");
         System.out.println("hello hotfix222!");
         System.out.println("hello master 222!");
+        System.out.println("github-push");
+
     }
 }
